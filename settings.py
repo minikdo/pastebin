aa58a5052681cb58b3088042e45100ffde873a4d
Dominik Szmek <do@minik.pl>
2019-09-15 13:02:12 +0200

    fix get_secret() indent

--- settings.py
+++ settings.py
@@ -7,14 +7,15 @@ with open(os.path.join(BASE_DIR, 'secrets.json')) as f:
     """ parse configuration file """
     secrets = json.loads(f.read())
 
-    def get_secret(setting, secrets=secrets):
-        """ get the secret variable or return explicit exception. """
-        try:
-            return secrets[setting]
-        except KeyError:
-            error_msg = 'Set the {0} environment variable'.format(setting)
-            print(error_msg)
-
+def get_secret(setting, secrets=secrets):
+    """ get the secret variable or return explicit exception. """
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = 'Set the {0} environment variable'.format(setting)
+        print(error_msg)
+        
+        
 SECRET_KEY = get_secret('SECRET_KEY')
 HOST = get_secret('HOST')
 UPLOAD_FOLDER = os.path.join(BASE_DIR, get_secret('UPLOAD_FOLDER'))
